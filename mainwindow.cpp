17f4cb63195063623d1baab03823155a46afcb1d
Al Williams <al.williams@awce.com>
2014-05-05 08:50:12 -0500

    Cosmetic clean ups

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,28 @@
+/*
+ * balooctl - Monitor and control baloo file indexing
+    Copyright (C) 2014  Al Williams al.williams@awce.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Yes this should use QtDbus but this is a very quick hack. I may or may not
+fix it later.
+*/
+
+// Polling time in seconds
+#define SCAN_TIME 10
+
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QTimer>
@@ -13,7 +38,7 @@ MainWindow::MainWindow(QWidget *parent) :
     updateresult();
     QTimer *timer = new QTimer(this);
      connect(timer, SIGNAL(timeout()), this, SLOT(updateresult()));
-     timer->start(10000);
+     timer->start(SCAN_TIME*1000);
 }
 
 
